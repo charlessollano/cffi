48b7801b77da3b8f738d19c836d30416f1b9cca3
franklin caruana <franklincaruana@gmail.com>
2022-11-30 03:13:51 +0800

    change filename from MemberState to GlobalState

--- src/components/CRUD/Member/AddMembers.js
+++ src/components/CRUD/Member/AddMembers.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState } from "react";
 import { useHistory } from "react-router-dom";
-import { MemberContext } from "../../../context/MemberState";
+import { MemberContext } from "../../../context/GlobalState";
 import DemoNavbar from "components/Navbars/DemoNavbar";
 import ReactDatetime from "react-datetime";
 import {
